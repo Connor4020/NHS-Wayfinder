52c8e442936cec8d1047dc1e8775d1f9e8bda755
liuandyhui1231 <Andy.Liu2@student.shu.ac.uk>
2026-02-20 10:12:01 +0000

    Fix Navbar Mobile

--- app/components/header.vue
+++ app/components/header.vue
@@ -28,43 +28,47 @@ const logout = async () => {
     <header class="header">
         <div id="Topbar">
             <div class="logo">
-              <img src="/resources/nhs_logo.jpg" alt="NHS Wayfinder Logo">
-                            <p class="logo-text">Northern General Hospital</p>
+                <img src="/resources/nhs_logo.jpg" alt="NHS Wayfinder Logo">
+                <p class="logo-text">Northern General Hospital</p>
             </div>
-                        <div class="topbar-actions">
-                                <GoogleTranslate />
-                                <div class="account">
-                                        <NuxtLink v-if="!user" to="/login">Login</NuxtLink>
-                                        <NuxtLink v-else to="#" @click.prevent="logout">
-                                                Logout
-                                        </NuxtLink>
-                                </div>
+            <div class="topbar-actions">
+                <GoogleTranslate />
+                <div class="account">
+                    <NuxtLink v-if="!user" to="/login">Login</NuxtLink>
+                    <NuxtLink v-else to="#" @click.prevent="logout">
+                        Logout
+                    </NuxtLink>
+                </div>
             </div>
         </div>
         <nav class="navbar">
             <ul>
-                <li id="link1"><NuxtLink to="/">Home</NuxtLink></li>
-                <li id="link2"><NuxtLink to="/find-location">Find Location</NuxtLink></li>
-                <li id="link3"><NuxtLink to="/about">About</NuxtLink></li>
-                <li id="link4"><NuxtLink to="/contact">Contact</NuxtLink></li>
-                <li><NuxtLink to="/admin/media">Media</NuxtLink></li>
-                <li><NuxtLink to="/admin/bfs-debug">BFS Debug</NuxtLink></li>
-                <li><NuxtLink to="/admin/dashboard">Dashboard</NuxtLink></li>
-                <li><NuxtLink to="/admin/node">Add Node</NuxtLink></li>
-                <li><NuxtLink to="/admin/connections">Connections</NuxtLink></li>
-                <li><NuxtLink to="/admin/users"> Users</NuxtLink></li>
+                <li><NuxtLink to="/">Home</NuxtLink></li>
+                <li><NuxtLink to="/find-location">Find Location</NuxtLink></li>
+                <li id="hidelink"><NuxtLink to="/about">About</NuxtLink></li>
+                <li id="hidelink"><NuxtLink to="/contact">Contact</NuxtLink></li>
+                <li v-if="user" id="hidelink"><NuxtLink to="/admin/bfs-debug">BFS Debug</NuxtLink></li>
+                <li v-if="user" id="hidelink"><NuxtLink to="/admin/media">Media</NuxtLink></li>
+                <li v-if="user" id="hidelink"><NuxtLink to="/admin/dashboard">Dashboard</NuxtLink></li>
+                <li v-if="user" id="hidelink"><NuxtLink to="/admin/node">Add Node</NuxtLink></li>
+                <li v-if="user" id="hidelink"><NuxtLink to="/admin/connections">Connections</NuxtLink></li>
+                <li v-if="user" id="hidelink"><NuxtLink to="/admin/users"> Users</NuxtLink></li>
                 <li id="Morebtn" @click.prevent="toggleDropdown"><a>More</a></li>
             </ul>
         </nav>
         <div class="dropdown" v-show="isDropdownOpen">
             <ul>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
                 <li v-if="!user"><a href="/login">Login</a></li>
-                <li v-if="user"><NuxtLink to="/admin/add-node">Add Node</NuxtLink></li>
-                <li v-if="user"><NuxtLink to="/admin/assign-media">Assign Media</NuxtLink></li>
                 <li v-if="user"><a href="#" @click.prevent="logout">Logout</a></li>
+                <li><NuxtLink to="/find-location">Find Location</NuxtLink></li>
+                <li><NuxtLink to="/about">About</NuxtLink></li>
+                <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+                <li v-if="user"><NuxtLink to="/admin/bfs-debug">BFS Debug</NuxtLink></li>
+                <li v-if="user"><NuxtLink to="/admin/media">Media</NuxtLink></li>
+                <li v-if="user"><NuxtLink to="/admin/dashboard">Dashboard</NuxtLink></li>
+                <li v-if="user"><NuxtLink to="/admin/node">Add Node</NuxtLink></li>
+                <li v-if="user"><NuxtLink to="/admin/connections">Connections</NuxtLink></li>
+                <li v-if="user"><NuxtLink to="/admin/users"> Users</NuxtLink></li>
             </ul>
         </div>
     </header>
