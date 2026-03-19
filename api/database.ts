1074f7ffc699ef98e4315de18b341cf13ab7469d
josephfoster05 <josephfoster2005@gmail.com>
2026-03-19 11:09:52 +0000

    DB STUFF

--- api/database.ts
+++ api/database.ts
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '10.70.197.16', // update to the host of the database
-  user: process.env.DB_USER || 'TEST1', // create a user to have access to the database
-  password: process.env.DB_PASS || 'AndyPushedToMainAgain123', // the password for the user
-  database: process.env.DB_NAME || 'mysql', // the name of the database to connect to
+  host: process.env.DB_HOST
+  user: process.env.DB_USER 
+  password: process.env.DB_PASS 
+  database: process.env.DB_NAME 
   waitForConnections: true, 
   connectionLimit: 10,
   queueLimit: 0
